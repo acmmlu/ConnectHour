a431753800a6d3f3705c78cc8263c4002e02b564
Luke <30377055+acmmlu@users.noreply.github.com>
2019-11-02 16:44:44 -0400

    Update globals.js

--- globals.js
+++ globals.js
@@ -1,7 +1,17 @@
 const mysql = require('mysql2');
 const nodemailer = require('nodemailer');
 const url = require('url');
+const {google} = require('googleapis');
 
+const OAuth2 = google.auth.OAuth2;
+const oath2Client = new OAuth2(
+    '926515268553-702nh6okk23jl79j8dkikfe2sliopt1r.apps.googleusercontent.com',
+    'jNlAfC11q9dPa1gm-Ofqq9h9',
+    'https://developers.google.com/oauthplayground'
+);
+oauth2Client.setCredentials({
+   refresh_token: '1//04_JmqiS7Xo-eCgYIARAAGAQSNwF-L9IrUhUUSvzFm8GF4HahCYbZxWcW0wM5EHjcFFHIz783TQKMbNIg0pxTE-CoTaqivC5Hdbo' 
+});
 
 // Create a connection to DB
 exports.pool = mysql.createPool(process.env.DATABASE_URL);
@@ -9,11 +19,13 @@ exports.pool = mysql.createPool(process.env.DATABASE_URL);
 exports.transport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        type: 'oauth2',
+        type: 'OAuth2',
         user: 'connecthourofficial@gmail.com',
         clientId: '926515268553-702nh6okk23jl79j8dkikfe2sliopt1r.apps.googleusercontent.com',
         clientSecret: 'jNlAfC11q9dPa1gm-Ofqq9h9',
         refreshToken: '1//04_JmqiS7Xo-eCgYIARAAGAQSNwF-L9IrUhUUSvzFm8GF4HahCYbZxWcW0wM5EHjcFFHIz783TQKMbNIg0pxTE-CoTaqivC5Hdbo',
+        accessToken: oauth2Client.getAccessToken(),
+        expires: (new Date()).getTime()
     }
 });
 
