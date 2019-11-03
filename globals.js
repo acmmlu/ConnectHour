32400e66a61f89c6e4ae4f64b8f7f43e03aba127
Luke <30377055+acmmlu@users.noreply.github.com>
2019-11-03 12:51:32 -0500

    Update globals.js

--- globals.js
+++ globals.js
@@ -5,12 +5,12 @@ const {google} = require('googleapis');
 
 const OAuth2 = google.auth.OAuth2;
 const oauth2Client = new OAuth2(
-    '926515268553-bb97lrsb78c4d3ms5auuuu33sq6vdr2i.apps.googleusercontent.com',
-    'HrETbf3xcyhGLU-R4pH_ma9b',
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
     'https://developers.google.com/oauthplayground'
 );
 oauth2Client.setCredentials({
-   refresh_token: '1//04_JmqiS7Xo-eCgYIARAAGAQSNwF-L9IrUhUUSvzFm8GF4HahCYbZxWcW0wM5EHjcFFHIz783TQKMbNIg0pxTE-CoTaqivC5Hdbo' 
+   refresh_token: process.env.REFRESH_TOKEN
 });
 
 // Create a connection to DB
@@ -21,9 +21,9 @@ exports.transport = nodemailer.createTransport({
     auth: {
         type: 'OAuth2',
         user: 'connecthourofficial@gmail.com',
-        clientId: '926515268553-bb97lrsb78c4d3ms5auuuu33sq6vdr2i.apps.googleusercontent.com',
-        clientSecret: 'HrETbf3xcyhGLU-R4pH_ma9b',
-        refreshToken: '1//04Sy1I7rretquCgYIARAAGAQSNwF-L9Irc0AamoaF6wH1Rssv671PU3KXswEt7Z1tvNvWj5LQi_79o05LBsREChBKPfgUupO4z1o',
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
         accessToken: oauth2Client.getAccessToken(),
         expires: (new Date()).getTime()
     }
