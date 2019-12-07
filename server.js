52d71f84c61a594539046c226a40725bd23f5669
Milan Chheta <michheta@iu.edu>
2019-12-07 14:56:11 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 const http = require('http');
-const url = require('url');
 const path = require('path');
 const express = require('express');
-const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const g = require('./globals');
 
 const app = express();
-const port = 40951;
+const port = process.env.PORT || 40951;
+
 
 app.use(express.static(path.join(__dirname, 'react_app', 'public')));
 app.use(fileUpload({limits: { fileSize: 65535}}));
@@ -17,13 +16,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const server = app.listen(port,  function () {
+const server = app.listen(port, "0.0.0.0", function () {
     console.log('Navigate to silo.sice.indiana.edu:'+port);
 });
 
 require('./routes')(app);
 
-// Render home page for any route not specified in routes.js
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'react_app', 'public', 'index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'react_app', 'build')));
+
+    // Render home page for any route not specified in routes.js
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'react_app', 'build', 'index.html'));
+    });
+} else {
+    app.use(express.static(path.join(__dirname, 'react_app', 'public')));
+}
