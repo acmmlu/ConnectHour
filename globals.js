1a9262e23104eabae65d304a5aac601df6cd0e02
lalovett <lalovett@iu.edu>
2019-12-07 15:23:42 -0500

    reverted global to previous state

--- globals.js
+++ globals.js
@@ -1,37 +1,31 @@
 const mysql = require('mysql2');
 const nodemailer = require('nodemailer');
-const {google} = require('googleapis');
 const url = require('url');
+const {google} = require('googleapis');
+
 const OAuth2 = google.auth.OAuth2;
 const oauth2Client = new OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
     'https://developers.google.com/oauthplayground'
 );
-
 oauth2Client.setCredentials({
    refresh_token: process.env.REFRESH_TOKEN
 });
+
 // Create a connection to DB
+exports.pool = mysql.createPool(process.env.DATABASE_URL);
 
-
-
-exports.google = google;
-exports.pool = mysql.createPool({
-    multipleStatements: true,
-    connectionLimit: 1000,
-    user: "p565f19_lalovett",
-    host: "db.sice.indiana.edu",
-    password: "my+sql=p565f19_lalovett",
-    database: "p565f19_lalovett"
-});
 exports.transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: 'gmail',
     auth: {
-        user: "connecthourofficialv2@gmail.com",
-        pass: "Connect@Hour2"
+        type: 'OAuth2',
+        user: 'connecthourofficial@gmail.com',
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: oauth2Client.getAccessToken(),
+        expires: (new Date()).getTime()
     }
 });
 
