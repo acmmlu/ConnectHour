2e938030a91d87ff3afe3515b83965c156e1ed70
Milan Chheta <michheta@iu.edu>
2019-12-07 15:06:13 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const g = require('./globals');
-
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = process.env.PORT || 40951;
 
